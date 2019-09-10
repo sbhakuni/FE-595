aa8f928ff7ad1dad37da09667d6b49a94ce52ceb
sbhakuni <48243680+sbhakuni@users.noreply.github.com>
2019-09-10 19:47:56 -0400

    Update 595HW1.py
    one period change

--- 595HW1.py
+++ 595HW1.py
@@ -8,7 +8,7 @@ Created on Fri Aug 30 23:47:23 2019
 import matplotlib.pyplot as plt
 import numpy as np
 #number_of_pi=8
-def sincosplot(number_of_pi=8,increment=0.1):
+def sincosplot(number_of_pi=2,increment=0.1):
     x = np.arange(0,number_of_pi*np.pi,increment)   # start,stop,step
     y = np.sin(x)   
     z = np.cos(x)
