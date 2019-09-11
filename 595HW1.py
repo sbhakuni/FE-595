c78cae99909bc4b615e9ebd3fba0342477371e94
sbhakuni <48243680+sbhakuni@users.noreply.github.com>
2019-09-11 00:19:44 -0400

    Final Code self commit

--- 595HW1.py
+++ 595HW1.py
@@ -7,11 +7,17 @@ Created on Fri Aug 30 23:47:23 2019
 
 import matplotlib.pyplot as plt
 import numpy as np
-#number_of_pi=8
+
 def sincosplot(number_of_pi=2,increment=0.1):
-    x = np.arange(0,number_of_pi*np.pi,increment)   # start,stop,step
+    x = np.arange(0,number_of_pi*np.pi,increment)
     y = np.sin(x)   
     z = np.cos(x)
+    plt.plot(x,y,x,z,)
     plt.plot(x,y,x,z)
+    plt.xlabel('x values for one period') 
+    plt.ylabel('sin(x) and cos(x)')
+    plt.title('Plot of sin(x) and cos(x) for one period(0 to 2pi)')
+    plt.legend(['sin(x)', 'cos(x)'])  
     plt.show()
+
 sincosplot()
