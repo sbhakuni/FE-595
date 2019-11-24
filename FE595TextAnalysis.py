c74d2737c50b29fd21afaa98a5dc87f3e0de02d1
sbhakuni <48243680+sbhakuni@users.noreply.github.com>
2019-11-23 19:00:17 -0500

    Update FE595TextAnalysis.py

--- FE595TextAnalysis.py
+++ FE595TextAnalysis.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# SHAANTANU BHAKUNI
-
-# (1) Used the results of the web scrappers made by individual students in the class to clean the data.
-# All results of Male and female characters were put in different text file and then analysis was dne on them
-
-# In[1]:
-
-
 import os, zipfile,glob
 
 dir_name = 'C:\\Users\\bhaku\\Desktop\\allheroesJ'
@@ -47,7 +36,7 @@ for i,x in enumerate(t_path):
 
     if term in words: #see if one of the words in the sentence is the word we want
         t_path.pop(i)
-    
+#have to do it twice because of the format of macosx  
 term = "__MACOSX" # AS MAC OS GIVES ADDITIONAL FILES WHILE ZIPPING WHICH ARE NOT ENCODED PROPERLY.WE WILL REMOVE IT
 index=[]
 for i,x in enumerate(t_path):
@@ -66,30 +55,22 @@ with open("result.txt") as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content]
+file= open("FemaleOutput.txt", 'a')
+file1= open("MaleOutput.txt", 'a')
 for i in content:
     if i.find('She')is not -1: 
-         file= open("FemaleOutput.txt", 'a')
          file.write(f"{i}\n")
 
     elif i.find('He')is not -1:
-         file= open("MaleOutput.txt", 'a')
-         file.write((f"{i}\n"))
-
-
-# (3) Sentiment analysis after cleaning the files(using textblob)
-
-# In[2]:
-
-
+         file1.write((f"{i}\n"))
+            
+            
 #   Sentiment analysis
 #for male heroes
 from textblob import TextBlob   
 import pandas as pd    
 fh = open('MaleOutput.txt')
 df_male=pd.DataFrame()
-for line in fh:
-    print(line)
-fh.close()
 df_male = pd.read_csv('MaleOutput.txt', sep="\n", header=None)
 df_male.columns=["Text"]
     
@@ -101,8 +82,7 @@ df_male["subjectivity"]=df_male["textblob"].apply(lambda x :x.sentiment[1])
 #for female heroes
 fh = open('FemaleOutput.txt')
 df_female=pd.DataFrame()
-for line in fh:
-    print(line)
+
 fh.close()
 df_female = pd.read_csv('FemaleOutput.txt', sep="\n", header=None)
 df_female.columns=["Text"]
@@ -125,47 +105,18 @@ Top10_positive_female=df_female.head(10)
 df_female=df_female.sort_values(by='polarity')
 Top10_negative_female=df_female.head(10)
 
-
-# In[11]:
-
-
 Top10_positive_male=Top10_positive_male.drop(columns=["textblob"])
+Top10_positive_male.drop_duplicates(subset ="Text", 
+                     keep = False, inplace = True) 
 Top10_negative_male=Top10_negative_male.drop(columns=["textblob"])
+Top10_positive_male.drop_duplicates(subset ="Text", 
+                     keep = False, inplace = True) 
 Top10_positive_female=Top10_positive_female.drop(columns=["textblob"])
+Top10_positive_female.drop_duplicates(subset ="Text", 
+                     keep = False, inplace = True) 
 Top10_negative_female=Top10_negative_female.drop(columns=["textblob"])
-
-
-# In[13]:
-
-
-print("Top 10 positive male characters are:")
-Top10_positive_male
-
-
-# In[14]:
-
-
-print("Top 10 negative male characters are:")
-Top10_negative_male
-
-
-# In[15]:
-
-
-print("Top 10 positive female characters are:")
-Top10_positive_female
-
-
-# In[16]:
-
-
-print("Top 10 negative  female characters are:")
-Top10_negative_female
-
-
-# (4)find the top 10 descriptors with their frequency 
-
-# In[17]:
+Top10_positive_female.drop_duplicates(subset ="Text", 
+                     keep = False, inplace = True) 
 
 
 from collections import Counter 
@@ -184,8 +135,7 @@ for i in range(0,len(qaz)):
 
 Counter = Counter(decriptors_series)
 # most_common() produces k frequently encountered 
-# input values and their respective counts. 
+ 
 most_occur = Counter.most_common(10) 
-  
+# input values and their respective counts.  
 print(most_occur)
-
