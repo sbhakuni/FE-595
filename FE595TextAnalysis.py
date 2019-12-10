0f4a0dc98bb2671d234ebe100c6b2347d40eea4c
sbhakuni <48243680+sbhakuni@users.noreply.github.com>
2019-12-10 01:41:24 -0500

    Update FE595TextAnalysis.py

--- FE595TextAnalysis.py
+++ FE595TextAnalysis.py
@@ -1,9 +1,24 @@
-import os, zipfile,glob
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 16:25:26 2019
 
-dir_name = 'C:\\Users\\bhaku\\Desktop\\allheroesJ'
+@author: bhaku
+"""
+
+import os, zipfile,glob
+from collections import Counter
+import sys
+from os.path import join, abspath, realpath, dirname
+
+import os
+from textblob import TextBlob   
+import pandas as pd  
+
+dir_name = os.path.dirname(os.path.abspath('__file__'))
+print (dir_name)
+
 extension = ".zip"
-os.chdir(dir_name)
-#unzip all files
+
 zip_files = glob.glob('*.zip')
 
 for zip_filename in zip_files:
@@ -11,24 +26,25 @@ for zip_filename in zip_files:
     os.mkdir(dir_name)
     zip_handler = zipfile.ZipFile(zip_filename, "a")
     zip_handler.extractall(dir_name)
-    
-test = os.listdir(dir_name)
+ #delete zip files after extraction
+'''for item in os.listdir(dir_name): # loop through items in dir
+    if item.endswith(extension): # check for ".zip" extension
+        file_name = os.path.abspath(item) # get full path of files
+        os.remove(file_name) # delete zipped file'''
 
-for item in test:
+
+for item in os.path.dirname("__file__"):
     if item.endswith(".zip"):
         os.remove(os.path.join(dir_name, item))
 
-text_path=[]    
+t_path=[]    
 for path, subdirs, files in os.walk(dir_name):
     
     for name in files:
         #text_path=[]
-        text_path.append(os.path.join(path, name))    
-        #print(name)
-t_path = [w.replace( '\\' , '\\\\' ) for w in text_path]#backlash changed for putting in directory
+        t_path.append(os.path.join(path, name))    
+        print(name)
 
-
-  
 term = "__MACOSX" # AS MAC OS GIVES ADDITIONAL FILES WHILE ZIPPING WHICH ARE NOT ENCODED PROPERLY.WE WILL REMOVE IT
 index=[]
 for i,x in enumerate(t_path):
@@ -36,62 +52,60 @@ for i,x in enumerate(t_path):
 
     if term in words: #see if one of the words in the sentence is the word we want
         t_path.pop(i)
-#have to do it twice because of the format of macosx  
+    
 term = "__MACOSX" # AS MAC OS GIVES ADDITIONAL FILES WHILE ZIPPING WHICH ARE NOT ENCODED PROPERLY.WE WILL REMOVE IT
 index=[]
 for i,x in enumerate(t_path):
-    words = x.split('\\\\') #split the sentence into individual words based on \\
+    words = x.split('\\\\') #split the sentence into individual words
 
     if term in words: #see if one of the words in the sentence is the word we want
         t_path.pop(i)
         
-with open("result.txt", 'w') as outfile:
-    for fname in t_path:
-        with open(fname) as infile:
-            outfile.write(infile.read())   
 
-           
 with open("result.txt") as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content]
-file= open("FemaleOutput.txt", 'a')
-file1= open("MaleOutput.txt", 'a')
+she='She\'s'
+he='He\'s'
 for i in content:
     if i.find('She')is not -1: 
+         file= open("FemaleOutput.txt", 'a')
          file.write(f"{i}\n")
 
     elif i.find('He')is not -1:
-         file1.write((f"{i}\n"))
-            
-            
+         file= open("MaleOutput.txt", 'a')
+         file.write((f"{i}\n"))
+
 #   Sentiment analysis
 #for male heroes
 from textblob import TextBlob   
 import pandas as pd    
 fh = open('MaleOutput.txt')
-df_male=pd.DataFrame()
-df_male = pd.read_csv('MaleOutput.txt', sep="\n", header=None)
-df_male.columns=["Text"]
-    
-    
-df_male["textblob"]=df_male["Text"].apply(lambda x:TextBlob(x)) 
-df_male["polarity"]=df_male["textblob"].apply(lambda x :x.sentiment[0])
-df_male["subjectivity"]=df_male["textblob"].apply(lambda x :x.sentiment[1])
 
-#for female heroes
-fh = open('FemaleOutput.txt')
-df_female=pd.DataFrame()
-
-fh.close()
-df_female = pd.read_csv('FemaleOutput.txt', sep="\n", header=None)
-df_female.columns=["Text"]
+def maledf(textfile):
+    #fh = open(textfile)
+    df_male=pd.DataFrame()
     
+    df_male = pd.read_csv(textfile, sep="\n", header=None)
+    df_male.columns=["Text"]
+    df_male["textblob"]=df_male["Text"].apply(lambda x:TextBlob(x)) 
+    df_male["polarity"]=df_male["textblob"].apply(lambda x :x.sentiment[0])
+    df_male["subjectivity"]=df_male["textblob"].apply(lambda x :x.sentiment[1])
+    return df_male
+df_male=maledf('MaleOutput.txt')
+def femaledf(textfile):
+    #fh = open(textfile)
+    df_female=pd.DataFrame()
     
-df_female["textblob"]=df_female["Text"].apply(lambda x:TextBlob(x)) 
-df_female["polarity"]=df_female["textblob"].apply(lambda x :x.sentiment[0])
-df_female["subjectivity"]=df_female["textblob"].apply(lambda x :x.sentiment[1])
+    df_female = pd.read_csv(textfile, sep="\n", header=None)
+    df_female.columns=["Text"]
+    df_female["textblob"]=df_female["Text"].apply(lambda x:TextBlob(x)) 
+    df_female["polarity"]=df_female["textblob"].apply(lambda x :x.sentiment[0])
+    df_female["subjectivity"]=df_female["textblob"].apply(lambda x :x.sentiment[1])
+    return df_female
 
+df_female=femaledf('FemaleOutput.txt')
 df_male=df_male.sort_values(by='polarity', ascending=False)
 df_female=df_female.sort_values(by='polarity', ascending=False)
 
@@ -105,37 +119,23 @@ Top10_positive_female=df_female.head(10)
 df_female=df_female.sort_values(by='polarity')
 Top10_negative_female=df_female.head(10)
 
-Top10_positive_male=Top10_positive_male.drop(columns=["textblob"])
-Top10_positive_male.drop_duplicates(subset ="Text", 
-                     keep = False, inplace = True) 
-Top10_negative_male=Top10_negative_male.drop(columns=["textblob"])
-Top10_positive_male.drop_duplicates(subset ="Text", 
-                     keep = False, inplace = True) 
-Top10_positive_female=Top10_positive_female.drop(columns=["textblob"])
-Top10_positive_female.drop_duplicates(subset ="Text", 
-                     keep = False, inplace = True) 
-Top10_negative_female=Top10_negative_female.drop(columns=["textblob"])
-Top10_positive_female.drop_duplicates(subset ="Text", 
-                     keep = False, inplace = True) 
-
-
-from collections import Counter 
-
-with open('result.txt', 'r') as file:
-    datastring = file.read().replace('\n', '')
-# split() returns list of all the words in the string 
+# Python program to find the k most frequent words 
+   
+def Counter1(filename):
+    with open(filename, 'r') as file:
+        datastring = file.read().replace('\n', '')
+    # split() returns list of all the words in the string 
+        
+    blob=TextBlob(datastring)
+    qaz=blob.noun_phrases
     
-blob=TextBlob(datastring)
-qaz=blob.noun_phrases
+    decriptors_series=[]
+    for i in range(0,len(qaz)):    
+        decriptors_series.append(qaz[i])         
+    Counter = Counter(decriptors_series) 
+    most_occur = Counter.most_common(10) 
+    return most_occur 
 
-decriptors_series=[]
-for i in range(0,len(qaz)):    
-    decriptors_series.append(qaz[i])
-    
+a=Counter1('result.txt')
+print(a)
 
-Counter = Counter(decriptors_series)
-# most_common() produces k frequently encountered 
- 
-most_occur = Counter.most_common(10) 
-# input values and their respective counts.  
-print(most_occur)
